e1c302d0bc911e51810450eaad1e0a99b139534b
Jon A <jon.jumangi@gmail.com>
2023-07-14 09:58:02 -0500

    Tutor help 7/13

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,93 @@
+const answer = document.getElementById("answ");
+const starter = document.querySelector("#starter");
+var scoreEl = document.getElementById('score');
+var timeEl = document.getElementById('time');
+const questionsEl = document.getElementById("quest");
+const mainContainer = document.getElementById("main-container");
 var interval
 var qIndex = 0
+var score = 0;
+let userResult
+var secondsLeft = 30
+var q;
+var a;
+var scoresArray = []
 
-
-const answer = document.getElementById("answ");
-
-const questions = [{
-  q: "What is does HTML stand for?",
-  a: [{ text: "Houston Texas Mountain Leauge", isCorrect: false },
-  { text: "Hyper Timid Mighty Lakes", isCorrect: false },
-  { text: "Hyper Text Markup Language", isCorrect: true },
-  { text: "Hypar Taxt Merkup Lenguega", isCorrect: false },
-  ]
-},
-{
-  q: "What does CSS stand for?",
-  a: [{ text: "California Standard Style", isCorrect: false },
-  { text: "Coool Style Standards", isCorrect: false },
-  { text: "Clairvoyant Spectators Supreme", isCorrect: false },
-  { text: "Cascading Style Sheets", isCorrect: true },
-  ]
-},
-{
-  q: "Which came first? Java or JavaScript?",
-  a: [{ text: "Java", isCorrect: true },
-  { text: "JavaScript", isCorrect: false },
-  { text: "Neither, the are the exact same age", isCorrect: false },
-  ]
-}
+const questions = [
+  {
+    q: "What is does HTML stand for?",
+    a: [{ text: "Houston Texas Mountain Leauge", isCorrect: false },
+    { text: "Hyper Timid Mighty Lakes", isCorrect: false },
+    { text: "Hyper Text Markup Language", isCorrect: true },
+    { text: "Hypar Taxt Merkup Lenguega", isCorrect: false },
+    ]
+  },
+  {
+    q: "What does CSS stand for?",
+    a: [{ text: "California Standard Style", isCorrect: false },
+    { text: "Coool Style Standards", isCorrect: false },
+    { text: "Clairvoyant Spectators Supreme", isCorrect: false },
+    { text: "Cascading Style Sheets", isCorrect: true },
+    ]
+  },
+  {
+    q: "Which came first? Java or JavaScript?",
+    a: [{ text: "Java", isCorrect: true },
+    { text: "JavaScript", isCorrect: false },
+    { text: "Neither, the are the exact same age", isCorrect: false },
+    ]
+  }
 ]
 
 
-let userResult
+function showGameOverEl() {
+  mainContainer.innerHTML = ""
+  const inputEl = document.createElement("input");
+  const btnEl = document.createElement("button");
+  btnEl.textContent = "Add Initials";
+  inputEl.classList.add(inputInitialsStyle);
+  btnEl.classList.add(btnInitialsStyle);
+  btnEl.addEventListener('click', addUserScoreToLocal())
+  mainContainer.append(inputEl)
+  mainContainer.append(btnEl)
+}
 
+function addUserScoreToLocal() {
+  // get the user current user score and initials and save to object
+  const userInitials = inputEl.textContent;
+  const userScore = score;
 
-const starter = document.querySelector("#starter");
+  const userObject = {
+    userInitials,
+    userScore
+  }
 
-starter.addEventListener("click", function () {                   //this will begin game once the button 'start quiz' is clicked
-  timer()
-  starter.classList.add("hide")
-  currentScore()
-  nextQuestion()
+  // add the object to the array
+  scoresArray = scoresArray.append(userObject);
 
+  // stringify the object and save to local
+  localStorage.setItem("savedScores", JSON.stringify(scoresArray))
+}
 
-});
-
-function nextQuestion() {                                               //this will load the next question
-  const questionsEl = document.getElementById("quest");
+//this will load the next question
+function nextQuestion() {
   answer.innerHTML = ""
 
-  var q = questions[qIndex].q
-  var a = questions[qIndex].a
+  // let's check if the qIndex value is greater than the array length - 1
+  // if it is, then show some other screen, element, whatever, end the game.
+  // otherwise don't do anything
+
+  if (qIndex > questions.length - 1) {
+    showGameOverEl();
+    // run some custom function here to show the field for entering initials and score
+    // then return null like below
+    return null;
+  }
+
+
+  q = questions[qIndex].q
+  a = questions[qIndex].a
+
   questionsEl.textContent = q
   for (let i = 0; i < a.length; i++) {
     const buttonEl = document.createElement("button")
@@ -57,66 +95,55 @@ function nextQuestion() {                                               //this w
     buttonEl.dataset.correct = a[i].isCorrect
     answer.append(buttonEl)
   }
-
-
 }
 
-var scoreEl = document.getElementById('score');
-var score = 0;
-
-function currentScore() {                                            //this will track and display the current score
-  answer.innerHTML = ""
-
-} if (isCorrect = { isCorrect: true }) {
-  score = score +=
-    scoreEl.textContent = ("Your score is:" + score);
-
-}
-
-
-
-var timeEl = document.getElementById('time');
-var secondsLeft = 100
-
+//this will track and display the current score
+// function currentScore() {
+//   answer.innerHTML = ""
+//   if (isCorrect = { isCorrect: true }) {
+//     score +=
+//       scoreEl.textContent = " " + score;
+//   }
+// }
 
 function timer() {                                                   //this will track the time left in the game
-  inteval = setInterval(function () {
+  interval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft + " seconds left ";
     if (secondsLeft <= 0) {
       clearInterval(interval)
-      alert("You game has run out of time... please try again")
+      alert("You game has run out of time... please refresh the page to try again")
+      //allow for user to save score thus far
     }
   }, 1000
   )
 
 }
-function saveScore() {                                                      //keep working (update local storage )
-  var stringify = JSON.stringify(scores)
-  localStorage.setItem("score", stringified)
 
+function saveScore() {                                                      // (update local storage )
+  const stringify = JSON.stringify(scores)
+  localStorage.setItem("score", stringifiedScores)
 }
-console.log(saveScore)
+
 function getScore() {
-  var stringedValue = localStorage.getItem(scores)
-  if (stringedValue) {
-
-
-    score = JSON.parse(stringedValue)
-  }
+  const existingScoresArray = JSON.parse(localStorage.getItem("savedScores"));
+  scoresArray = scoresArray.append(...existingScoresArray);
 }
-function answerHandler(event) {                                 //if the user get's an incorrect answer, time will be deducted
+
+//if the user get's an incorrect answer, time will be deducted
+function answerHandler(event) {
   console.log(event.target.dataset.correct)
 
   if (event.target.dataset.correct === "false") {
-    secondsLeft = secondsLeft - 10
-
+    secondsLeft = secondsLeft - 10;
+  } else {
+    score += 1; // score = score + 1
   }
-  qIndex++
+
+  qIndex++;
+  // update the score element with the value of your variable 'score'
+  scoreEl.textContent = "Your score is" + score + ".";
   nextQuestion()
-
-
-
 }
 
 function enterInitials() {
@@ -125,3 +152,12 @@ function enterInitials() {
 
 
 answer.addEventListener("click", answerHandler)
+starter.addEventListener("click", function () {                   //this will begin game once the button 'start quiz' is clicked
+  timer()
+  starter.classList.add("hide")
+  currentScore()
+  nextQuestion()
+});
+
+// run getScores when this file is read by the brwoswer
+getScore();
